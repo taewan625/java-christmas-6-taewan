f590737648314b4e9edd84fa48623a5d06801063
taewan625 <taewan1997625@gmail.com>
2023-11-10 06:20:15 +0900

    Feat: Add isBadge(), getBadge()

---
+++ src/main/java/christmas/model/domain/Badge.java
@@ -0,0 +1,29 @@
+package christmas.model.domain;
+
+public enum Badge implements Comparable<Badge> {
+    STAR(5_000),
+    TREE(10_000),
+    SANTA(20_000);
+
+    private static final int MIN_EVENT_POINT = 5000;
+
+    private final int eventPoint;
+
+    Badge(int eventPoint) {
+        this.eventPoint = eventPoint;
+    }
+
+    public static boolean isBadge(int eventPoint) {
+        return eventPoint >= MIN_EVENT_POINT;
+    }
+
+    public static Badge getBadge(int eventPoint) {
+        Badge getBadge = null;
+        for (Badge value : Badge.values()) {
+            if (value.eventPoint <= eventPoint) {
+                getBadge = value;
+            }
+        }
+        return getBadge;
+    }
+}
