021a1e9086ee18f39b7e3b91ada13e7a526c9e35
taewan625 <taewan1997625@gmail.com>
2023-11-10 04:56:59 +0900

    Feat: Add DurationEvent & getDurationDiscount(), isDurationEvent(), applyDurationDiscount()

---
+++ src/main/java/christmas/model/domain/DurationEvent.java
@@ -0,0 +1,23 @@
+package christmas.model.domain;
+
+public class DurationEvent {
+    private final int INIT_DISCOUNT = 1000;
+    private final int INCREASE_PRICE = 100;
+    private final int START_DATE = 1;
+    private final int FINISH_DATE = 25;
+
+    private int durationDiscount = 0;
+
+    public int getDurationDiscount(int date) {
+        durationDiscount = INIT_DISCOUNT + ((date - 1) * INCREASE_PRICE);
+        return durationDiscount;
+    }
+
+    public boolean isDurationEvent(int date) {
+        return START_DATE <= date && date <= FINISH_DATE;
+    }
+
+    public int applyDurationDiscount(int totalPrice) {
+        return totalPrice - durationDiscount;
+    }
+}
