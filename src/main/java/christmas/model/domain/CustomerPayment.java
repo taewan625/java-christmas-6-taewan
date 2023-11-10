19675f7453c3316c608ee1c7169d1e07364507dc
taewan625 <taewan1997625@gmail.com>
2023-11-10 20:47:09 +0900

    Refactor: Add 고객 결제 로직

--- src/main/java/christmas/model/domain/CustomerPayment.java
+++ src/main/java/christmas/model/domain/CustomerPayment.java
@@ -3,4 +3,14 @@ package christmas.model.domain;
 public class CustomerPayment {
     private int totalOrderPrice;
     private int totalPaymentPrice;
+
+    public CustomerPayment(int totalOrderPrice, int totalPaymentPrice) {
+        this.totalOrderPrice = totalOrderPrice;
+        this.totalPaymentPrice = totalPaymentPrice;
+    }
+
+    public int calculateTotalPaymentPrice(int discountPrice) {
+        totalPaymentPrice -= discountPrice;
+        return totalPaymentPrice;
+    }
 }
