4feb64384122910bc8d694ad1cdbdd6aef3aeff3
taewan625 <taewan1997625@gmail.com>
2023-11-10 13:00:20 +0900

    Refactor: Fix getDDayDiscount() 반환 타입, iv 및 검증기능 제거
    검증기능은 Date enum에서 처리하도록 변경

--- src/main/java/christmas/model/domain/DDayEvent.java
+++ src/main/java/christmas/model/domain/DDayEvent.java
@@ -1,24 +1,15 @@
 package christmas.model.domain;
 
-public class DDay {
-    private final String DURATION = "크리스마스 디데이 할인";
+import java.util.Map;
+
+public class DDayEvent {
+    private final String D_DAY = "크리스마스 디데이 할인";
     private final int INIT_DISCOUNT = 1000;
     private final int INCREASE_PRICE = 100;
-    private final int START_DATE = 1;
-    private final int FINISH_DATE = 25;
 
-    private int durationDiscount = 0;
 
-    public int getDurationDiscount(int date) {
-        durationDiscount = INIT_DISCOUNT + ((date - 1) * INCREASE_PRICE);
-        return durationDiscount;
-    }
-
-    public boolean isDurationEvent(int date) {
-        return START_DATE <= date && date <= FINISH_DATE;
-    }
-
-    public int applyDurationDiscount(int totalPrice) {
-        return totalPrice - durationDiscount;
+    public Map<String, Integer> getDDayDiscount(int date) {
+        int dDayDiscount = INIT_DISCOUNT + ((date - 1) * INCREASE_PRICE);
+        return Map.of(D_DAY, dDayDiscount);
     }
 }
