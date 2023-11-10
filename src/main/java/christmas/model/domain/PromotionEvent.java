6c2d906f249cc581cef8aeeeba595ee3196ba478
taewan625 <taewan1997625@gmail.com>
2023-11-10 13:08:39 +0900

    Refactor: Fix getPromotion() 반환타입 및 iv 제거

--- src/main/java/christmas/model/domain/PromotionEvent.java
+++ src/main/java/christmas/model/domain/PromotionEvent.java
@@ -1,13 +1,17 @@
 package christmas.model.domain;
 
+import java.util.Map;
+
 public class PromotionEvent {
     private final String PROMOTION = "샴페인 1개";
-    private final int PROMOTION_PRICE = 120_000;
+    private final int PROMOTION_PRICE = 25_000;
+    private final int PROMOTION_LIMIT_PRICE = 120_000;
 
     public boolean isPromotionEvent(int totalPrice) {
-        return totalPrice >= PROMOTION_PRICE;
+        return totalPrice >= PROMOTION_LIMIT_PRICE;
     }
 
-    // todo 주문서에는 true일 경우 String과 int price를 줘야할텐데 이건 다음 단계 구현 윤곽 잡히면 그 때 메서드 생성
-    //  ex) Map <String eventType, Map<String promotion, Integer price>>
+    public Map<String, Integer> getPromotion() {
+        return Map.of(PROMOTION, PROMOTION_PRICE);
+    }
 }
