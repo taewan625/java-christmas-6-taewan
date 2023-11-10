0a916ea68c38a43738751c3ccbdf2c531b1f63ac
taewan625 <taewan1997625@gmail.com>
2023-11-10 13:10:09 +0900

    Test: Fix 소스코드에 맞춰 수정

--- src/test/java/christmas/model/domain/StarEventTest.java
+++ src/test/java/christmas/model/domain/StarEventTest.java
@@ -3,14 +3,17 @@ package christmas.model.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.Map;
+
 class StarEventTest {
 
     @Test
-    void applyStarDayDiscount() {
+    void getWeekdayDiscount() {
         StarEvent starEvent = new StarEvent();
-        int totalPrice = 10000;
-        int discountedAmount = starEvent.applyStarDayDiscount(totalPrice);
-        Assertions.assertThat(discountedAmount).isEqualTo(9000);
+        Map<String, Integer> starDayDiscount = starEvent.getStarDayDiscount();
+        Collection<Integer> values = starDayDiscount.values();
+        Assertions.assertThat(values.contains(1000)).isTrue();
 
     }
 }
