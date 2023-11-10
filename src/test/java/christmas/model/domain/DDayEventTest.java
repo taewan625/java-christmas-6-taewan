e89d39e0f5fed2908f330d21ad171e681097e27a
taewan625 <taewan1997625@gmail.com>
2023-11-10 13:09:25 +0900

    Test: Fix 소스코드에 맞춰 수정

--- src/test/java/christmas/model/domain/DDayEventTest.java
+++ src/test/java/christmas/model/domain/DDayEventTest.java
@@ -2,38 +2,19 @@ package christmas.model.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-class DDayTest {
-    DDay DDay = new DDay();
+class DDayEventTest {
+    DDayEvent DDayEvent = new DDayEvent();
 
     @Test
-    void getDurationDiscount() {
+    void getDDayDiscount() {
         int reservationDate = 1;
-        int durationDiscount = DDay.getDurationDiscount(reservationDate);
-        assertThat(durationDiscount).isEqualTo(1000);
-    }
-
-    @Test
-    void isDurationEvent() {
-        int reservationDate1 = 1;
-        int reservationDate2 = 25;
-        int reservationDate3 = 26;
-
-        boolean durationEvent1 = DDay.isDurationEvent(reservationDate1);
-        boolean durationEvent2 = DDay.isDurationEvent(reservationDate2);
-        boolean durationEvent3 = DDay.isDurationEvent(reservationDate3);
-        assertThat(durationEvent1).isTrue();
-        assertThat(durationEvent2).isTrue();
-        assertThat(durationEvent3).isFalse();
-    }
-
-    @Test
-    void applyDurationDiscount() {
-        int totalPrice = 11000;
-        int date = 3;
-        int durationDiscount = DDay.getDurationDiscount(date);
-        int discountPrice = DDay.applyDurationDiscount(totalPrice);
-        assertThat(discountPrice).isEqualTo(9800);
+        Map<String, Integer> dDayDiscount = DDayEvent.getDDayDiscount(reservationDate);
+        Collection<Integer> values = dDayDiscount.values();
+        assertThat(values.contains(1000)).isTrue();
     }
 }
