efbd68f949e51c03dac70276faaf07e482dc60ff
taewan625 <taewan1997625@gmail.com>
2023-11-10 20:46:44 +0900

    Refactor: Add 고객 할인 정보, 증정품 정보 분리, 혜택할인 금액 로직

--- src/main/java/christmas/model/domain/CustomerEvent.java
+++ src/main/java/christmas/model/domain/CustomerEvent.java
@@ -1,9 +1,30 @@
 package christmas.model.domain;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerEvent {
-    private Map<String, Integer> eventDatas;
+    private Map<String, Integer> discountEventDatas;
+    private Map<String, Integer> promotionEventDatas;
+    private int totalDiscountPrice;
     private int totalBenefitAmount;
     private Badge badge;
+
+    public CustomerEvent() {
+        this.discountEventDatas = new HashMap<>();
+        this.promotionEventDatas = new HashMap<>();
+    }
+
+    public void setEventData() {
+        totalDiscountPrice = sumEventValues(discountEventDatas);
+        totalBenefitAmount = totalDiscountPrice + sumEventValues(promotionEventDatas);
+    }
+
+    private int sumEventValues(Map<String, Integer> datas) {
+        return datas.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public void setBadge() {
+        badge = Badge.getBadge(totalBenefitAmount);
+    }
 }
