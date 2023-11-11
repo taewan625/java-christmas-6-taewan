603a5826cbf14c89ee7bb968d2bd7c378ec54629
taewan625 <taewan1997625@gmail.com>
2023-11-12 08:37:35 +0900

    Refactor: Fix class -> enum 및 method 추가

--- src/main/java/christmas/model/domain/Promotion.java
+++ src/main/java/christmas/model/domain/Promotion.java
@@ -2,16 +2,22 @@ package christmas.model.domain;
 
 import java.util.Map;
 
-public class PromotionEvent {
-    private final String PROMOTION = "샴페인 1개";
-    private final int PROMOTION_PRICE = 25_000;
-    private final int PROMOTION_LIMIT_PRICE = 120_000;
+public enum Promotion {
+    DRINK_3("증정 이벤트", 1);
+    private static final int PROMOTION_LIMIT_PRICE = 120_000;
+    private final String type;
+    private final int count;
 
-    public boolean isPromotionEvent(int totalPrice) {
+    Promotion(String type, int count) {
+        this.type = type;
+        this.count = count;
+    }
+
+    public static boolean isPromotion(int totalPrice) {
         return totalPrice >= PROMOTION_LIMIT_PRICE;
     }
 
-    public Map<String, Integer> getPromotion() {
-        return Map.of(PROMOTION, PROMOTION_PRICE);
+    public static Map<String, Integer> getPromotion() {
+        return Map.of(DRINK_3.name(), DRINK_3.count);
     }
 }
