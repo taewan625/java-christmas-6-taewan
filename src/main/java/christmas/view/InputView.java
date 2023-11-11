fee1c4e739581f6938af63013736185821f0a424
taewan625 <taewan1997625@gmail.com>
2023-11-12 08:29:42 +0900

    Feat: Add InputView

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,12 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String reserveDate() {
+        return Console.readLine();
+    }
+    public static String orderMenu() {
+        return Console.readLine();
+    }
+}
