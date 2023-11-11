16aa1b3c818cb06d7825664282473a521dad25a2
taewan625 <taewan1997625@gmail.com>
2023-11-12 08:35:57 +0900

    Refactor: Change DiscountEvent -> Discount로 변수, 매개변수 이름

--- src/main/java/christmas/model/domain/Discount.java
+++ src/main/java/christmas/model/domain/Discount.java
@@ -2,7 +2,7 @@ package christmas.model.domain;
 
 import java.util.Map;
 
-public enum DiscountEvent {
+public enum Discount {
     D_DAY("크리스마스 디데이 할인", 1000),
     WEEKDAY("평일 할인", 2023),
     WEEKEND("주말 할인", 2023),
@@ -12,56 +12,52 @@ public enum DiscountEvent {
     private final String type;
     private final int discount;
 
-    DiscountEvent(String type, int discount) {
+    Discount(String type, int discount) {
         this.type = type;
         this.discount = discount;
     }
 
-    public static DiscountEvent getDiscountEvent(String discountType) {
-        return DiscountEvent.valueOf(discountType);
+    public static Map<String, Integer> applyDiscount(String discountType, int date, Map<Menu, Integer> orderMenu) {
+        Discount discount = Discount.valueOf(discountType);
+        if (discount == D_DAY) {
+            return Discount.dDayDiscount(date, discount);
+        }
+        if (discount == WEEKDAY) {
+            return Discount.mainDishDiscount(orderMenu, discount);
+        }
+        if (discount == WEEKEND) {
+            return Discount.dessertDiscount(orderMenu, discount);
+        }
+        return Discount.starDayDiscount(discount);
     }
 
-    public static Map<String, Integer> chooseDiscount(DiscountEvent discountEvent, int date,
-                                                      Map<Menu, Integer> orderMenu) {
-        if (discountEvent == D_DAY) {
-            return DiscountEvent.dDayDiscount(date, discountEvent);
-        }
-        if (discountEvent == WEEKDAY) {
-            return DiscountEvent.mainDishDiscount(orderMenu, discountEvent);
-        }
-        if (discountEvent == WEEKEND) {
-            return DiscountEvent.dessertDiscount(orderMenu, discountEvent);
-        }
-        return DiscountEvent.starDayDiscount(discountEvent);
+    public static Map<String, Integer> dDayDiscount(int date, Discount discount) {
+        int dDayDiscount = discount.discount + ((date - 1) * INCREASE_PRICE);
+        return Map.of(discount.type, dDayDiscount);
     }
 
-    public static Map<String, Integer> dDayDiscount(int date, DiscountEvent discountEvent) {
-        int dDayDiscount = discountEvent.discount + ((date - 1) * INCREASE_PRICE);
-        return Map.of(discountEvent.type, dDayDiscount);
-    }
-
-    public static Map<String, Integer> mainDishDiscount(Map<Menu, Integer> orderMenu, DiscountEvent discountEvent) {
+    public static Map<String, Integer> mainDishDiscount(Map<Menu, Integer> orderMenu, Discount discount) {
         int mainDishDiscount = 0;
         for (Menu menu : orderMenu.keySet()) {
             if (Menu.isMain(menu)) {
-                mainDishDiscount += orderMenu.get(menu) * discountEvent.discount;
+                mainDishDiscount += orderMenu.get(menu) * discount.discount;
             }
         }
-        return Map.of(discountEvent.type, mainDishDiscount);
+        return Map.of(discount.type, mainDishDiscount);
     }
 
-    public static Map<String, Integer> dessertDiscount(Map<Menu, Integer> orderMenu, DiscountEvent discountEvent) {
+    public static Map<String, Integer> dessertDiscount(Map<Menu, Integer> orderMenu, Discount discount) {
         int dessertDiscount = 0;
         for (Menu menu : orderMenu.keySet()) {
             if (Menu.isDessert(menu)) {
-                dessertDiscount += orderMenu.get(menu) * discountEvent.discount;
+                dessertDiscount += orderMenu.get(menu) * discount.discount;
             }
         }
-        return Map.of(discountEvent.type, dessertDiscount);
+        return Map.of(discount.type, dessertDiscount);
     }
 
-    public static Map<String, Integer> starDayDiscount(DiscountEvent discountEvent) {
-        return Map.of(discountEvent.type, discountEvent.discount);
+    public static Map<String, Integer> starDayDiscount(Discount discount) {
+        return Map.of(discount.type, discount.discount);
     }
 
 }
