a71ba84321d355b82d7e251650fef278d06f409e
taewan625 <taewan1997625@gmail.com>
2023-11-12 14:38:18 +0900

    Refactor: Add 혜택내역 method

--- src/main/java/christmas/model/domain/CustomerEvent.java
+++ src/main/java/christmas/model/domain/CustomerEvent.java
@@ -1,6 +1,12 @@
 package christmas.model.domain;
 
+import christmas.util.XmasConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CustomerEvent {
     private Map<String, Integer> discountEventDatas;
@@ -44,5 +50,20 @@ public class CustomerEvent {
         // promotion 종류가 1개기 때문에 next()만 호출
         String promotionEventDataKey = promotionEventDatas.keySet().iterator().next();
         return promotionEventDataKey;
+    }
+
+    public List<String> getDiscountData() {
+        return discountEventDatas.entrySet()
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isPromotionEventDatas() {
+        return !promotionEventDatas.isEmpty();
+    }
+
+    public boolean isDiscountEventDatas() {
+        return !discountEventDatas.isEmpty();
     }
 }
