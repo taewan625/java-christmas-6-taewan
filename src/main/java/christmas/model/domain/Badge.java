b9bbb809a49b7db67a8c5a9223a013f3aa273b49
taewan625 <taewan1997625@gmail.com>
2023-11-12 20:23:19 +0900

    Refactor: Fix badge method

--- src/main/java/christmas/model/domain/Badge.java
+++ src/main/java/christmas/model/domain/Badge.java
@@ -1,29 +1,31 @@
 package christmas.model.domain;
 
 public enum Badge {
-    STAR(5_000),
-    TREE(10_000),
-    SANTA(20_000);
+    STAR(5_000, "별"),
+    TREE(10_000, "트리"),
+    SANTA(20_000, "산타");
 
     private static final int MIN_EVENT_POINT = 5000;
 
     private final int eventPoint;
+    private final String name;
 
-    Badge(int eventPoint) {
+    Badge(int eventPoint, String name) {
         this.eventPoint = eventPoint;
+        this.name = name;
     }
 
     public static boolean isBadge(int eventPoint) {
         return eventPoint >= MIN_EVENT_POINT;
     }
 
-    public static Badge getBadge(int eventPoint) {
-        Badge getBadge = null;
+    public static String getBadge(int eventPoint) {
+        String badgeName = "없음";
         for (Badge value : Badge.values()) {
             if (value.eventPoint <= eventPoint) {
-                getBadge = value;
+                badgeName = value.name;
             }
         }
-        return getBadge;
+        return badgeName;
     }
 }
