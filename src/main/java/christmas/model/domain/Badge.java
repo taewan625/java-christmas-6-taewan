89fe8ff6b5b0fbe1fd92dc9871d100f7b28ab228
taewan625 <taewan1997625@gmail.com>
2023-11-14 14:56:29 +0900

    Refactor: Fix Badge가 String을 반환, Add "없음" 상수

--- src/main/java/christmas/model/domain/Badge.java
+++ src/main/java/christmas/model/domain/Badge.java
@@ -1,11 +1,15 @@
 package christmas.model.domain;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public enum Badge {
     STAR(5_000, "별"),
     TREE(10_000, "트리"),
     SANTA(20_000, "산타");
 
     private static final int MIN_EVENT_POINT = 5000;
+    private static final String NONE = "없음";
 
     private final int eventPoint;
     private final String name;
@@ -19,13 +23,11 @@ public enum Badge {
         return eventPoint >= MIN_EVENT_POINT;
     }
 
-    public static String getBadge(int eventPoint) {
-        String badgeName = "없음";
-        for (Badge value : Badge.values()) {
-            if (value.eventPoint <= eventPoint) {
-                badgeName = value.name;
-            }
-        }
-        return badgeName;
+    public static String getEventBadgeName(int eventPoint) {
+        return Arrays.stream(values())
+                .filter(badge -> badge.eventPoint <= eventPoint)
+                .max(Comparator.comparingInt(badge -> badge.eventPoint))
+                .map(badge -> badge.name)
+                .orElse(NONE);
     }
 }
