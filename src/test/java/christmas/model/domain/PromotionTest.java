2dac347b9f1bb41d9d161337167151a271528026
taewan625 <taewan1997625@gmail.com>
2023-11-15 11:35:26 +0900

    Test: Fix promotion
    promotion은 menu에 많은 의존을 한다.
    getPromotionMenus()는 menu의 method로 넣었는데 이는 목적이 menu를 보여주는 것에 가깝기 때문

--- src/test/java/christmas/model/domain/PromotionTest.java
+++ src/test/java/christmas/model/domain/PromotionTest.java
@@ -1,16 +1,26 @@
 package christmas.model.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class PromotionEventTest {
+import java.util.Map;
+
+import static christmas.model.domain.Promotion.DRINK_3;
+import static christmas.model.domain.Promotion.isPromotion;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PromotionTest {
 
     @Test
     void isPromotionEvent() {
-        PromotionEvent promotionEvent = new PromotionEvent();
-        Assertions.assertThat(promotionEvent.isPromotionEvent(120_000)).isTrue();
-        Assertions.assertThat(promotionEvent.isPromotionEvent(240_000)).isTrue();
-        Assertions.assertThat(promotionEvent.isPromotionEvent(119_999)).isFalse();
-        Assertions.assertThat(promotionEvent.isPromotionEvent(100_000)).isFalse();
+        assertThat(isPromotion(120_000)).isEqualTo(Map.of(DRINK_3, 1));
+        assertThat(isPromotion(240_000)).isEqualTo(Map.of(DRINK_3, 1));
+        assertThat(isPromotion(119_999)).isEmpty();
+        assertThat(isPromotion(100_000)).isEmpty();
+    }
+
+    @Test
+    void setPromotionDatas() {
+        assertThat(Promotion.setPromotionDatas(Map.of(DRINK_3, 1)))
+                .isEqualTo(Map.of("증정 이벤트", 25000));
     }
 }
