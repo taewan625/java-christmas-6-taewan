6f33cdb48acb1020f5ea41869f3e2733841098fd
taewan625 <taewan1997625@gmail.com>
2023-11-15 11:09:45 +0900

    Refacotr: Remove 중복 메서드, Fix menuData() - 공통 사용 메서드

--- src/main/java/christmas/util/XmasConverter.java
+++ src/main/java/christmas/util/XmasConverter.java
@@ -15,7 +15,7 @@ public class XmasConverter {
     private static final String FORMAT_COMMA = ",";
     private static final String FORMAT_EQUAL = "=";
     private static final String FORMAT_COLON = ":";
-    private static final String SPACE = " ";
+    private static final String FORMAT_SPACE = " ";
 
     private static final String MONTH = "12월 ";
     private static final String DATE_UNIT = "일";
@@ -26,33 +26,36 @@ public class XmasConverter {
     private static final int TYPE = 0;
     private static final int PRICE = 1;
 
-    public static Map<Menu, Integer> orders(String orders) {
-        return getOrderDatas(orders)
-                .collect(Collectors.toMap(
-                        orderData -> Menu.getMenu(orderData[MENU]),
-                        orderData -> Integer.parseInt(orderData[COUNT])
-                ));
-    }
-
-    public static Stream<String[]> getOrderDatas(String orders) {
+    // 검증 methods
+    public static Stream<String[]> splitOrderDatas(String orders) {
         return Arrays.stream(orders.split(FORMAT_COMMA))
                 .map(order -> order.split(FORMAT_HYPHEN));
     }
 
+    private static Stream<String> splitOrderDatas(String orders, int menu) {
+        return Arrays.stream(orders.split(FORMAT_COMMA))
+                .map(order -> order.split(FORMAT_HYPHEN)[menu]);
+    }
+
+
     public static Set<Menu> orderMenus(String orders) {
-        return getOrderDatas(orders, MENU)
+        return splitOrderDatas(orders, MENU)
                 .map(Menu::getMenu)
                 .collect(Collectors.toSet());
     }
 
     public static List<String> orderMenusCounts(String orders) {
-        return getOrderDatas(orders, COUNT)
+        return splitOrderDatas(orders, COUNT)
                 .collect(Collectors.toList());
     }
 
-    private static Stream<String> getOrderDatas(String orders, int menu) {
-        return Arrays.stream(orders.split(FORMAT_COMMA))
-                .map(order -> order.split(FORMAT_HYPHEN)[menu]);
+    // 수행 methods
+    public static Map<Menu, Integer> orders(String orders) {
+        return splitOrderDatas(orders)
+                .collect(Collectors.toMap(
+                        orderData -> Menu.getMenu(orderData[MENU]),
+                        orderData -> Integer.parseInt(orderData[COUNT])
+                ));
     }
 
     public static String dateToFullDate(int date) {
@@ -65,16 +68,27 @@ public class XmasConverter {
     }
 
     public static String toMinusWon(int discountPrice) {
+        if (discountPrice == 0) {
+            return toWon(discountPrice);
+        }
         DecimalFormat decimalFormat = new DecimalFormat("-#,###원");
         return decimalFormat.format(discountPrice);
     }
 
     public static String benefitData(String discountData) {
         String[] discountDatas = discountData.split(FORMAT_EQUAL);
-        return discountDatas[TYPE] + FORMAT_COLON + SPACE + toMinusWon(Integer.parseInt(discountDatas[PRICE]));
+        return discountDatas[TYPE]
+                + FORMAT_COLON
+                + FORMAT_SPACE
+                + toMinusWon(Integer.parseInt(discountDatas[PRICE]));
     }
 
-    public static String menuData(String name, Integer count) {
-        return name + SPACE + count + COUNT_UNIT;
+    public static List<String> menuData(Map<String, Integer> menus) {
+        return menus.entrySet().stream()
+                .map(menu -> menu.getKey()
+                        + FORMAT_SPACE
+                        + menu.getValue()
+                        + COUNT_UNIT)
+                .toList();
     }
 }
